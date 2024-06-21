00ddb1893d08c26b8791a4a997606561d227ec08
DV <dreamervulpi@gmail.com>
2024-06-21 16:16:53 +0300

    add user CRUD

--- internal/user/usercore/user.go
+++ internal/user/usercore/user.go
@@ -19,3 +19,15 @@ func New(storage *userstorage.UserStorage) *Core {
 func (c *Core) CreateUser(ctx context.Context, request *domain.CreateUserRequest) (*domain.CreateUserResponse, error) {
 	return &domain.CreateUserResponse{}, nil
 }
+
+func (c *Core) GetUser(ctx context.Context, request *domain.GetUserRequest) (*domain.GetUserResponse, error) {
+	return &domain.GetUserResponse{}, nil
+}
+
+func (c *Core) UpdateUser(ctx context.Context, request *domain.UpdateUserRequest) (*domain.UpdateUserResponse, error) {
+	return &domain.UpdateUserResponse{}, nil
+}
+
+func (c *Core) DeleteUser(ctx context.Context, request *domain.DeleteUserRequest) (*domain.DeleteUserResponse, error) {
+	return &domain.DeleteUserResponse{}, nil
+}
