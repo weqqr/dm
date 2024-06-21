478873d18066e88f5e76ad80dc8d7b1cb90bd04a
DV <dreamervulpi@gmail.com>
2024-06-21 16:16:53 +0300

    add user CRUD

--- internal/user/userservice/user.go
+++ internal/user/userservice/user.go
@@ -5,6 +5,7 @@ import (
 	"dm/internal/domain"
 	"dm/internal/rpc"
 	"dm/internal/user/usercore"
+
 	"google.golang.org/grpc"
 )
 
@@ -26,4 +27,16 @@ func (s *UserService) Register(server *grpc.Server) {
 
 func (s *UserService) CreateUser(ctx context.Context, request *domain.CreateUserRequest) (*domain.CreateUserResponse, error) {
 	return s.core.CreateUser(ctx, request)
+}
+
+func (s *UserService) GetUser(ctx context.Context, request *domain.GetUserRequest) (*domain.GetUserResponse, error) {
+	return s.core.GetUser(ctx, request)
+}
+
+func (s *UserService) UpdateUser(ctx context.Context, request *domain.UpdateUserRequest) (*domain.UpdateUserResponse, error) {
+	return s.core.UpdateUser(ctx, request)
+}
+
+func (s *UserService) DeleteUser(ctx context.Context, request *domain.DeleteUserRequest) (*domain.DeleteUserResponse, error) {
+	return s.core.DeleteUser(ctx, request)
 }
